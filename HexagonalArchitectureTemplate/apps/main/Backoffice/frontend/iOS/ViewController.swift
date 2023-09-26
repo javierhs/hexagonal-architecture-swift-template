94b5c89c847d8569f1ca4eef68d1b8d0e508d8ef
Javier Hernández Sansalvador <javierpwnned@gmail.com>
2023-09-26 12:33:12 +0200

    Attempt of commit

--- HexagonalArchitectureTemplate/apps/main/Backoffice/frontend/iOS/ViewController.swift
+++ HexagonalArchitectureTemplate/apps/main/Backoffice/frontend/iOS/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let rootView = ContentView()
+        let hostingController = UIHostingController(rootView: rootView)
+        
+        
         print("Holaaa")
     }
 
