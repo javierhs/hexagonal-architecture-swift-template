f03492e63e4b7493f8968a15b27f2fc7082fdcf3
Javier Hernández Sansalvador <javierpwnned@gmail.com>
2023-09-29 09:23:25 +0200

    Delete SwiftUIView example

--- HexagonalArchitectureTemplate/apps/main/Backoffice/frontend/iOS/ViewController.swift
+++ HexagonalArchitectureTemplate/apps/main/Backoffice/frontend/iOS/ViewController.swift
@@ -14,20 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let swiftUIView = SwiftUIView()
-        let controller = UIHostingController(rootView: swiftUIView)
-        addChild(controller)
-        controller.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(controller.view)
-        controller.didMove(toParent: self)
-
-        NSLayoutConstraint.activate([
-            controller.view.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            controller.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
-            controller.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            controller.view.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
         print("Holaaa")
     }
 
